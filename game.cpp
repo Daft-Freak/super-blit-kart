5e31410bb2ec00391a827e37e16eb1285c858c24
Charlie Birks <charlie@daft.games>
2021-02-23 22:01:24 +0000

    "Mode 7"
    This is more based on the TONC tutorial for GBA than the 32blit code...

--- game.cpp
+++ game.cpp
@@ -3,8 +3,29 @@
 
 using namespace blit;
 
+class Camera final {
+public:
+    void update() {
+        forward = look_at - pos;
+        forward.normalize();
+
+        right = Vec3(-forward.z, 0.0f, forward.x);
+
+        up = right.cross(forward);
+    }
+
+    Vec3 pos, look_at;
+
+    Vec3 forward, right, up;
+
+    float focal_distance = 320.0f;
+};
+
 static Surface *map_tiles;
 static TileMap *map;
+
+static Vec3 cam_base_pos(0.0f, 32.0f, 0.0f);
+static Camera cam;
 
 void load_tilemap() {
     // some of this should really be in the API...
@@ -36,13 +57,61 @@ void init() {
 
     map_tiles = Surface::load(asset_tiles);
     load_tilemap();
+
+    cam.pos = cam_base_pos;
+    cam.look_at = Vec3(64.0f, 0.0f, 64.0f);
+    cam.update();
+}
+
+float r = 0.0f;// + 90;
+
+Mat3 mode7_scanline_transform(uint8_t y) {
+    float top = screen.bounds.h / 2;
+    float left = -screen.bounds.w / 2;
+
+    // pitch
+    float pitch_c = cam.up.y;
+    float pitch_s = -cam.forward.y;
+
+    float yb = (y - top) * pitch_c + cam.focal_distance * pitch_s;
+    float zb = (y - top) * pitch_s - cam.focal_distance * pitch_c;
+
+    float l = cam.pos.y / yb;
+
+    // yaw
+    float scaled_yaw_c = l * cam.right.x;
+    float scaled_yaw_s = l * cam.right.z;
+
+    // final translation
+    float tx = cam.pos.x + scaled_yaw_c * left - scaled_yaw_s * zb;
+    float ty = cam.pos.z + scaled_yaw_s * left + scaled_yaw_c * zb;
+
+    Mat3 mat = Mat3::identity();
+
+    mat.v00 = scaled_yaw_c; mat.v10 = scaled_yaw_s;
+
+    mat.v11 = 0.0f;
+
+    mat.v02 = tx; mat.v12 = ty;
+
+    return mat;
 }
 
 void render(uint32_t time) {
+    screen.pen = Pen(0,0,0);
     screen.clear();
 
-    map->draw(&screen, Rect(Point(0, 0), screen.bounds));
+    map->draw(&screen, Rect(Point(0, 0), screen.bounds), mode7_scanline_transform);
 }
 
 void update(uint32_t time) {
+    r += 0.5f;
+
+    // move camera
+    cam.pos = cam_base_pos - cam.look_at;
+    cam.pos.transform(Mat4::rotation(r, Vec3(0,1,0)));
+    cam.pos += cam.look_at;
+
+    cam.pos.y += std::sin(r / 100.0f) * 16.0f;
+    cam.update();
 }
