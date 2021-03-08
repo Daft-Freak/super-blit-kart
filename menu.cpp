4993f2a0ed098482962e53eb8bfa6cb875dbb194
Charlie Birks <charlie@daft.games>
2021-03-08 13:31:13 +0000

    Add menu wrapper (from daftboy)

---
+++ menu.cpp
@@ -0,0 +1,28 @@
+#include "menu.hpp"
+
+#include "engine/engine.hpp"
+
+Menu::Menu(std::string_view title, std::vector<Item> items, const blit::Font &font) : blit::Menu(title, nullptr, 0, font), items_vec(std::move(items)) {
+    this->items = items_vec.data();
+    num_items = items_vec.size();
+
+    item_h = font.char_h + 2;
+    item_adjust_y = 0;
+
+    header_h = item_h;
+    footer_h = 0;
+    margin_y = 0;
+
+    background_colour = blit::Pen(0x11, 0x11, 0x11);
+    foreground_colour = blit::Pen(0xF7, 0xF7, 0xF7);
+    selected_item_background = blit::Pen(0x22, 0x22, 0x22);
+}
+
+void Menu::set_on_item_activated(void (*func)(const Item &)) {
+    on_item_pressed = func;
+}
+
+void Menu::item_activated(const Item &item) {
+    if(on_item_pressed)
+        on_item_pressed(item);
+}
---
+++ menu.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+#include "engine/menu.hpp"
+
+class Menu final : public blit::Menu {
+public:
+    Menu(std::string_view title, std::vector<Item> items, const blit::Font &font = blit::minimal_font);
+
+    void set_on_item_activated(void (*func)(const Item &));
+
+private:
+    void item_activated(const Item &item) override;
+
+    std::vector<Item> items_vec;
+
+    void (*on_item_pressed)(const Item &) = nullptr;
+};
