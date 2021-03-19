09c46165b3f00e6fa3a5b8add378a418b4ebde07
Charlie Birks <charlie@daft.games>
2021-03-19 14:27:00 +0000

    Initial time trial save data

--- save.hpp
+++ save.hpp
@@ -4,6 +4,7 @@
 
 enum class SaveType {
     RaceResult = 0,
+    TimeTrial = 1,
     UserData = 0xF
 };
 
@@ -15,6 +16,22 @@ struct RaceSaveData {
     uint8_t save_version = 1;
     uint8_t place;
     uint16_t time; // ms/10
+};
+
+struct TimeTrialSaveData {
+    uint8_t save_version = 1;
+    uint8_t pad = 0;
+    uint16_t lap_time[3];
+
+    char name[8]{0};
+
+    struct {
+        int16_t pos_x, pos_z;
+        int16_t look_ang;
+        uint16_t pad; // hmm
+    } ghost_data[4096];
+
+    uint16_t ghost_data_used = 0;
 };
 
 struct UserSaveData {
