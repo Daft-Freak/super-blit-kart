3abfaf04378f4c67b37dc39f8e8ed6c70288c2c1
Charlie Birks <charlie@daft.games>
2021-05-26 16:10:44 +0100

    Update map loading

--- track.cpp
+++ track.cpp
@@ -152,28 +152,6 @@ std::vector<Sprite3D> &Track::get_sprites() {
 }
 
 void Track::load_tilemap() {
-    // some of this should really be in the API...
-    #pragma pack(push,1)
-    struct TMX {
-        char head[4];
-        uint8_t empty_tile;
-        uint16_t width;
-        uint16_t height;
-        uint16_t layers;
-        uint8_t data[];
-    };
-    #pragma pack(pop)
-
-    auto map_struct = reinterpret_cast<const TMX *>(info.map_asset);
-    auto layer_size = map_struct->width * map_struct->height;
-
-    // const_cast the tile data (not going to modify it)
-    map = new TileMap(
-        const_cast<uint8_t *>(map_struct->data),
-        const_cast<uint8_t *>(map_struct->data + layer_size),
-        Size(map_struct->width, map_struct->height),
-        tiles
-    );
-
-    map->empty_tile_id = map_struct->empty_tile;
+    map = TileMap::load_tmx(info.map_asset, tiles, 0, 0);
+    map->repeat_mode = TileMap::CLAMP_TO_EDGE;
 }
