95b76e9c7464aaeeafc4656dd5d883478a65430a
Charlie Birks <charlie@daft.games>
2021-08-11 15:06:27 +0100

    Add a couple of smaller fonts

--- fonts.hpp
+++ fonts.hpp
@@ -3,8 +3,10 @@
 #include "assets.hpp"
 
 inline const blit::Font tall_font(asset_tall_font);
+inline const blit::Font eight_font(asset_eight_font);
 
 inline const blit::Font menu_font(asset_menu_font);
 
 // only numbers
+inline const blit::Font medium_number_font(asset_medium_number_font);
 inline const blit::Font big_number_font(asset_big_number_font);
